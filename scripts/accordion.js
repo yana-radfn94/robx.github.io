0827e9acd79757420cdae39e8a26086fe8ea7a44
Gleb Svechnikov <gleb.svechnikov@gmail.com>
2016-09-24 12:16:46 +0400

    Added accordion

---
+++ scripts/accordion.js
@@ -0,0 +1,11 @@
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
+
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+      $("#videoframe").attr('src', $(this).data('src')); //load according video
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+
+    });
+  });
